20c38561aec4f2fe91f30914b547a675e00e7b99
Ram-at <pathakv586@gmail.com>
2025-07-30 17:09:44 +0530

    Refactor useNowPlayingMovies hook to prevent unnecessary API calls when movies are already in store

--- src/hooks/useNowPlayingMovies.js
+++ src/hooks/useNowPlayingMovies.js
@@ -1,10 +1,14 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { API_OPTIONS } from "../utils/constants";
 import { addNowPlayingMovies } from "../utils/movieSlice";
 
 const useNowPlayingMovies = () => {
   const dispatch = useDispatch();
+
+  //This is made so that if movie is there on the store then don't make an api call this is made on every fetch so i have made here only but we can make this only every api call (this is memoization)
+  const moviesInStore = useSelector(store => store.movie.nowPlayingMovies);
+
   const getNowPlayingMovies = async () => {
     const data = await fetch(
       "https://api.themoviedb.org/3/movie/now_playing?page=1",
@@ -15,7 +19,8 @@ const useNowPlayingMovies = () => {
     dispatch(addNowPlayingMovies(json.results));
   };
   useEffect(() => {
-    getNowPlayingMovies();
+     //This is made so that if movie is there on the store then don't make an api call
+   !moviesInStore && getNowPlayingMovies();
   }, []);
 };
 export default useNowPlayingMovies;
